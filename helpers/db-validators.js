5f1572f6220f0b3cddc8e4a634707cc76c0a3af5
jonatan <jsimon@randstad.com.ar>
2021-02-24 21:35:09 -0300

    Agrego funcion en helpers para validacion de email como middleware

--- helpers/db-validators.js
+++ helpers/db-validators.js
@@ -1,4 +1,5 @@
 const Role = require('../models/role');
+const Usuario = require('../models/usuario');
 
 const esRoleValido = async(rol = '') => {
     const existeRol = await Role.findOne({ rol });
@@ -7,6 +8,17 @@ const esRoleValido = async(rol = '') => {
     }
 }
 
+const emailExiste = async(correo = '') => {
+
+    //Verificar si el correo existe
+    const existeEmail = await Usuario.findOne({ correo });
+    if (existeEmail) {
+        throw new Error(`El correo ${ correo } ya esta registrado`)
+    }
+}
+
+
 module.exports = {
-    esRoleValido
+    esRoleValido,
+    emailExiste
 }
--- routes/usuarios.js
+++ routes/usuarios.js
@@ -2,7 +2,7 @@ const { Router } = require('express');
 const { check } = require('express-validator');
 
 const { validarCampos } = require('../middlewares/validar-campos');
-const { esRoleValido } = require('../helpers/db-validators');
+const { esRoleValido, emailExiste } = require('../helpers/db-validators');
 
 const {
     usuariosGet,
@@ -22,7 +22,7 @@ router.put('/:id', usuariosPut);
 router.post('/', [
     check('nombre', 'El nombre es obligatorio').not().isEmpty(),
     check('password', 'El password debe de ser más de 6 letras').isLength({ min: 6 }),
-    check('correo', 'El correo no es válido').isEmail(),
+    check('correo').custom(emailExiste)
     // check('rol', 'No es rol permitido').isIn(['ADMIN_ROLE','USER_ROLE']),
     check('rol').custom(esRoleValido), //equivalente a: check('rol').custom( (rol) => esRoleValido(rol) ),
     validarCampos
